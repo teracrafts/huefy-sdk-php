7d97d2f2d9a3b2af7f8bdf03d7b04a9e4b4a4423
Kingsley Eze <firebox@ikingsley.com>
2026-04-24 20:57:00 +0200

    refact(php): rename BulkEmailResult.php to SendBulkEmailsResponse.php for PSR-4 compatibility

--- src/Models/SendBulkEmailsResponse.php
+++ src/Models/SendBulkEmailsResponse.php
@@ -4,15 +4,6 @@ declare(strict_types=1);
 
 namespace Huefy\Models;
 
-class BulkRecipient
-{
-    public function __construct(
-        public readonly string $email,
-        public readonly string $type = 'to',
-        public readonly ?array $data = null,
-    ) {}
-}
-
 class SendBulkEmailsResponseData
 {
     public function __construct(
